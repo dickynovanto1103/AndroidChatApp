3aec9cb3efb762fda6f2be05b170cba6dab637a8
PratamaAgung <apratamamia@gmail.com>
2018-02-20 05:59:34 +0700

    add shaking animation to logo in ShakeIt

--- app/src/main/java/com/example/android/shakeandchat/ShakeIt.java
+++ app/src/main/java/com/example/android/shakeandchat/ShakeIt.java
@@ -2,6 +2,8 @@ package com.example.android.shakeandchat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 import com.example.android.shakeandchat.R;
 
@@ -11,5 +13,8 @@ public class ShakeIt extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shake_it);
+
+        Animation shake = AnimationUtils.loadAnimation(this, R.anim.shake);
+        findViewById(R.id.shake_logo).startAnimation(shake);
     }
 }
