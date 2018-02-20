e9ffa59b407f1c4608dc110de5f0bda855e1bd7e
PratamaAgung <apratamamia@gmail.com>
2018-02-20 12:15:34 +0700

    add shakelistener

--- app/src/main/java/com/example/android/shakeandchat/ShakeIt.java
+++ app/src/main/java/com/example/android/shakeandchat/ShakeIt.java
@@ -1,5 +1,8 @@
 package com.example.android.shakeandchat;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.animation.Animation;
@@ -8,13 +11,37 @@ import android.view.animation.AnimationUtils;
 import com.example.android.shakeandchat.R;
 
 public class ShakeIt extends AppCompatActivity {
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    private ShakeDetector mShakeDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shake_it);
 
-        Animation shake = AnimationUtils.loadAnimation(this, R.anim.shake);
-        findViewById(R.id.shake_logo).startAnimation(shake);
+        final Animation shake = AnimationUtils.loadAnimation(this, R.anim.shake);
+
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mShakeDetector = new ShakeDetector();
+        mShakeDetector.setOnShakeListener(new ShakeDetector.OnShakeListener() {
+            @Override
+            public void onShake(int count) {
+                findViewById(R.id.shake_logo).startAnimation(shake);
+            }
+        });
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        mSensorManager.registerListener(mShakeDetector, mAccelerometer, SensorManager.SENSOR_DELAY_UI);
+    }
+
+    @Override
+    public void onPause(){
+        mSensorManager.unregisterListener(mShakeDetector);
+        super.onPause();
     }
 }
