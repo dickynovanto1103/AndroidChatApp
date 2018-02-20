39fed901df65f83ed9fca286fd447970fe35c42c
PratamaAgung <apratamamia@gmail.com>
2018-02-20 19:10:44 +0700

    add status text

--- app/src/main/java/com/example/android/shakeandchat/ShakeIt.java
+++ app/src/main/java/com/example/android/shakeandchat/ShakeIt.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 import com.example.android.shakeandchat.R;
 
@@ -14,6 +15,7 @@ public class ShakeIt extends AppCompatActivity {
     private SensorManager mSensorManager;
     private Sensor mAccelerometer;
     private ShakeDetector mShakeDetector;
+    private TextView mStatusText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +23,7 @@ public class ShakeIt extends AppCompatActivity {
         setContentView(R.layout.activity_shake_it);
 
         final Animation shake = AnimationUtils.loadAnimation(this, R.anim.shake);
+        mStatusText = (TextView) findViewById(R.id.status_shake);
 
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
@@ -30,8 +33,10 @@ public class ShakeIt extends AppCompatActivity {
             public void onShake(int count) {
                 if (count > 0){
                     findViewById(R.id.shake_logo).startAnimation(shake);
+                    mStatusText.setText(R.string.shaking_status);
                 } else {
                     findViewById(R.id.shake_logo).clearAnimation();
+                    mStatusText.setText(R.string.waiting_shake_status);
                 }
             }
         });
