ef7d4cee0a4a34a1879e099b46e7c67cb37a8c54
PratamaAgung <apratamamia@gmail.com>
2018-02-27 14:53:15 +0700

    change listToken key

--- app/src/main/java/com/example/android/shakeandchat/MyFirebaseInstanceIDService.java
+++ app/src/main/java/com/example/android/shakeandchat/MyFirebaseInstanceIDService.java
@@ -3,6 +3,8 @@ package com.example.android.shakeandchat;
 import android.content.Intent;
 import android.util.Log;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.FirebaseInstanceIdService;
 
@@ -16,16 +18,16 @@ public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
     @Override
     public void onTokenRefresh() {
         //get updated InstanceID token
-//        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
-//        Log.d(TAG,"Refreshed token: " + refreshedToken);
-//
-//        sendRegistrationToServer(refreshedToken);
-        Log.d(TAG, "test");
-        Intent intent = new Intent(this, RegistrationIntentService.class);
-        startService(intent);
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG,"Refreshed token: " + refreshedToken);
+
+        sendRegistrationToServer(refreshedToken);
+//        Log.d(TAG, "test");
+//        Intent intent = new Intent(this, RegistrationIntentService.class);
+//        startService(intent);
     }
 
     private void sendRegistrationToServer(String token){
-        // TODO
+        DatabaseReference listToken = FirebaseDatabase.getInstance().getReference("listToken");
     }
 }
